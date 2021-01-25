2b3c7c7660ac1a6120ca6d26e2edcd8171689479
BRYAN\bryan <bryanherberth@gmail.com>
2021-01-25 19:41:17 +0700

    YES

--- APBD/app/src/main/java/com/example/apbd/MainActivity.kt
+++ APBD/app/src/main/java/com/example/apbd/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.apbd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private fun myToast(Pesan: String,Waktu: Int= Toast.LENGTH_SHORT)
+            = Toast.makeText(this,Pesan,Waktu)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.Home)
+        setContentView(R.layout.boot_up)
     }
+    fun showToast(view: View) {
+        myToast(Pesan = "testrun").show()
+    }
+
 }
