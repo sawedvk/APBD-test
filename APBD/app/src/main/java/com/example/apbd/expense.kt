094bbd7eef9bbe3ebc7cbaa91e20cca8761bbc60
DESKTOP-4T0R8CL\Sawe <vjstephanie22@gmail.com>
2021-01-28 21:49:35 +0700

    testing alert dialog

--- APBD/app/src/main/java/com/example/apbd/expense.kt
+++ APBD/app/src/main/java/com/example/apbd/expense.kt
@@ -1,11 +1,19 @@
 package com.example.apbd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class expense : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.expense)
+    }
+
+    fun alertKonfirmasi(view: View) {
+        var dialog = AlertDialog.Builder(this)
+            .setMessage("You Almost Exceeded Your Plan!")
+        dialog.show()
     }
 }
--- APBD/app/src/main/java/com/example/apbd/income.kt
+++ APBD/app/src/main/java/com/example/apbd/income.kt
@@ -1,10 +1,24 @@
 package com.example.apbd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class income : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun alertKonfirmasi(view: View){
+        var dialog = AlertDialog.Builder( this)
+            .setMessage("Save")
+            .setPositiveButton("savePlan", DialogInterface.OnClickListener{dialogInterface, i -> Toast.makeText(this,"SAVE AND PLAN NOW!", Toast.LENGTH_LONG).show()
+            })
+            .setNegativeButton("Cancel", DialogInterface.OnClickListener{dialogInterface, i -> Toast.makeText(this,"CANCEL", Toast.LENGTH_LONG).show()
+            })
+        dialog.show()
     }
 }
