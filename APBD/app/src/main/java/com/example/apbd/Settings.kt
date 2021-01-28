7c460af1f55cf043e03a215445ce1a5eed52fdb1
DESKTOP-4T0R8CL\Sawe <vjstephanie22@gmail.com>
2021-01-28 22:27:18 +0700

    alert dialog sudah d update

--- APBD/app/src/main/java/com/example/apbd/Settings.kt
+++ APBD/app/src/main/java/com/example/apbd/Settings.kt
@@ -1,11 +1,25 @@
 package com.example.apbd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_page)
+    }
+
+    fun alertKonfirmasi(view: View){
+        var dialog = AlertDialog.Builder( this)
+            .setMessage("Logout ?")
+            .setPositiveButton("Logout", DialogInterface.OnClickListener{ dialogInterface, i -> Toast.makeText(this,"LOGOUT", Toast.LENGTH_LONG).show()
+            })
+            .setNegativeButton("Logout", DialogInterface.OnClickListener{ dialogInterface, i -> Toast.makeText(this,"CANCEL", Toast.LENGTH_LONG).show()
+            })
+        dialog.show()
     }
 }
