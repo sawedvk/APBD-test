b12d1dbecea253a14fb4b7b19a53bd58d05c6c34
BRYAN\bryan <bryanherberth@gmail.com>
2021-03-08 23:34:49 +0700

    test 2

--- APBD/app/src/main/java/com/example/apbd/MainActivity.kt
+++ APBD/app/src/main/java/com/example/apbd/MainActivity.kt
@@ -9,15 +9,13 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private fun myToast(Pesan: String,Waktu: Int= Toast.LENGTH_SHORT)
-            = Toast.makeText(this,Pesan,Waktu)
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.boot_up)
     }
-    fun showToast(view: View) {
-        myToast(Pesan = "Login Succesful").show()
+    fun goToLogin(view: View) {
         var intentlogin = Intent(this,LoginPage::class.java)
         startActivity(intentlogin)
     }
