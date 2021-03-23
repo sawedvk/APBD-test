d78a0ec3b0b83a93a6f65c4f819126d5e4f6a3eb
BRYAN\bryan <bryanherberth@gmail.com>
2021-03-24 06:39:15 +0700

    Reciever Pt.1

---
+++ APBD/app/src/main/java/com/example/apbd/AirplaneReciever.kt
@@ -0,0 +1,13 @@
+package com.example.apbd
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AirplaneReciever : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("AirplaneReciever.onReceive() is not implemented")
+    }
+}
