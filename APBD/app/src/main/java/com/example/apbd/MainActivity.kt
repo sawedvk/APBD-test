b99d40a353af937f785639b0d851982f34009a09
BRYAN\bryan <bryanherberth@gmail.com>
2021-03-24 22:48:17 +0700

    erplane

--- APBD/app/src/main/java/com/example/apbd/MainActivity.kt
+++ APBD/app/src/main/java/com/example/apbd/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.apbd
 
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +12,10 @@ class MainActivity : AppCompatActivity(){
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.boot_up)
-
+        var AirRecieve = AirplaneReciever()
+        var filter = IntentFilter()
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(AirRecieve,filter)
 
     }
 
