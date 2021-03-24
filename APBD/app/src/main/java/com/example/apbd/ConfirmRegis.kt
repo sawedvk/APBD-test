e4f96585327d21e3167264b0c8e7fa76daead3bc
YUPITER\ASUS ROG <yupiternc@gmail.com>
2021-03-24 22:42:27 +0700

    OTP Done

--- APBD/app/src/main/java/com/example/apbd/ConfirmRegis.kt
+++ APBD/app/src/main/java/com/example/apbd/ConfirmRegis.kt
@@ -1,14 +1,23 @@
 package com.example.apbd
 
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.content.BroadcastReceiver
+import android.provider.Telephony
+
 
 class ConfirmRegis : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_otpcode)
+
+        var SMSReceive = SMSReceiver()
+        var filter = IntentFilter()
+        filter.addAction(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
+        registerReceiver(SMSReceive,filter)
     }
 
     fun goToLogin(view: View) {
