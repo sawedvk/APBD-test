425dd2760091d6f804c49690c5f9559943765b9d
BRYAN\bryan <bryanherberth@gmail.com>
2021-05-05 02:51:08 +0700

    semi

---
+++ APBD/app/src/main/java/com/example/apbd/SharedPref.kt
@@ -0,0 +1,10 @@
+package com.example.apbd
+
+import android.content.Context
+
+class SharedPref(context: Context, itemDetails : String) {
+    val Item_Name = "Product"
+    val Item_Date = "Date"
+    val Item_Price ="Price"
+
+}
